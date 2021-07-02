3d946b5f73c671da4a0cee29236fbfc35ed4e04e
Matheus Lucas <nerdola.cartola@gmail.com>
2021-07-02 12:50:30 -0300

    Creating URL for create-rooms

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -26,7 +26,7 @@ function handleClick (event, check = true) {
     const roomId = document.querySelector('#room-id').dataset.id
     const questionId = event.target.dataset.id
     const form = document.querySelector('.modal form')
-    form.setAttribute('action', `/room/${roomId}/${questionId}/${slug}`)
+    form.setAttribute('action', `/question/${roomId}/${questionId}/${slug}`)
     
     modalTitle.innerHTML = `${text} esta pergunta`
     modalDescription.innerHTML = `Tem certeza que deseja ${text.toLocaleLowerCase()} esta pergunta ?`
