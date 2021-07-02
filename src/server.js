3b9f7ecf060b5c974a4aceaa8b8164f567e992f9
Matheus Lucas <nerdola.cartola@gmail.com>
2021-07-02 12:11:31 -0300

    Create parts of ejs

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const route  = require('./route')
+const route = require('./route')
 const path = require('path')
 
 const server = express()
@@ -9,6 +9,8 @@ server.set('view engine', 'ejs')
 server.use(express.static('public'))
 
 server.set('views', path.join(__dirname, 'views'))
+
+server.use(express.urlencoded({extended: true}))
 
 server.use(route)
 
