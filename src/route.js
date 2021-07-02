054a2d19d8f0431f709ba2bb378d0019cfdd58c4
Matheus Lucas <nerdola.cartola@gmail.com>
2021-07-02 11:05:13 -0300

    Getting the information of the questions

--- src/route.js
+++ src/route.js
@@ -5,7 +5,6 @@ const route = express.Router()
 route.get('/', (req, res) => res.render('index'))
 route.get('/room', (req, res) => res.render('room'))
 route.get('/create-pass', (req, res) => res.render('create-pass'))
-
-
+// route.post('/question/:room/:question/:action', QuestionController.index)
 
 module.exports = route
