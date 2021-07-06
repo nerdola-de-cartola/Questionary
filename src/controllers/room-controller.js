fed09d0152cd694978aa120f172cac13dd74716e
Matheus Lucas <nerdola.cartola@gmail.com>
2021-07-06 00:31:43 -0300

    Verifica se o Id da sala é único

--- src/controllers/room-controller.js
+++ src/controllers/room-controller.js
@@ -4,25 +4,47 @@ module.exports = {
     async create(req, res) {
         const db = await Database()
         const password = req.body.password
+        let isRoom = true
+        while (isRoom) {
 
-        let roomId = Math.floor(Math.random() * 10)
-        
-        for (let i = 0; i < 5; i++) {
-            roomId += Math.floor(Math.random() * 10).toString()
+            // Gera o número da sala
+            let roomId = Math.floor(Math.random() * 10)
+
+            for (let i = 0; i < 5; i++) {
+                roomId += Math.floor(Math.random() * 10).toString()
+            }
+
+            // Pega os ids existentes
+            const roomsExistIds = await db.all(`
+                SELECT id FROM rooms
+            `)
+
+            // Verifica se o id já existe
+            isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+
+            // Insere o id no banco de dados se ele for inédito
+            if (!isRoom) {
+
+                await db.run(`
+                    INSERT INTO rooms (
+                        id,
+                        password
+                    )
+                    VALUES (
+                        ${parseInt(roomId)},
+                        ${password}
+                    )
+                `)
+            }
         }
 
-        await db.run(`
-            INSERT INTO rooms (
-                id,
-                password
-            )
-            VALUES (
-                ${parseInt(roomId)},
-                ${password}
-            )
-        `)
         await db.close()
 
         res.redirect(`/room/${roomId}`)
+    },
+
+    open(req, res) {
+        const roomId = req.params.room
+        res.render('room', { roomId: roomId })
     }
 }
