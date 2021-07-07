5eecf036763715426b2abe6f0ed4f80f3751a280
Matheus Lucas <nerdola.cartola@gmail.com>
2021-07-06 23:09:08 -0300

    Fixing bug in the generator of ids

--- src/controllers/room-controller.js
+++ src/controllers/room-controller.js
@@ -4,13 +4,15 @@ module.exports = {
     async create(req, res) {
         const db = await Database()
         const password = req.body.password
+        let roomId
         let isRoom = true
         while (isRoom) {
 
             // Gera o número da sala
-            let roomId = Math.floor(Math.random() * 10)
+            roomId = Math.floor(Math.random() * 10)
 
-            for (let i = 0; i < 5; i++) {
+            for (let i = 0; i < 6; i++) {
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
                 roomId += Math.floor(Math.random() * 10).toString()
             }
 
